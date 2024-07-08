516ac7d9597c457aff2a873fe83aa45485d920a3
kauxp <paramjeetkaur4326@gmail.com>
2024-07-08 12:27:33 +0530

    Added user end-point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const mongoose = require('mongoose');
 const productRouter = require('./routes/productRouter')
+const userRouter = require('./routes/userRouter')
 const app = express();
 require('dotenv').config();
 app.use(express.json());
@@ -14,7 +15,7 @@ mongoose.connect(process.env.MONGO_URL)
 })
 
 app.use('/api/products', productRouter);
-app.use('/api/users' , userRoutes)
+app.use('/api/users' , userRouter)
 
 app.listen(3030, () => {
     console.log('Server is running on port 3030');
